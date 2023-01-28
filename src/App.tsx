055d0059391caae488e946a847b7df4e50269dcb
Joseph <joseph161216@gmail.com>
2023-01-27 17:14:35 -0800

    Add react folder structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
+      inventore quisquam, vero minus ab recusandae accusantium aut maiores quia
+      incidunt quo officiis excepturi eveniet molestias, est esse repellendus
+      voluptatum. Ullam?
+    </>
+  );
+}
+
+export default App;
